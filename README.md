78715dfbbe4ac90b30ccb134f10902e78e71fc12
Skyven730 <jerom@DESKTOP-MSI-B450-GAMING-PLUS-MAX.localdomain>
2026-05-15 20:43:05 +0200

    Clean Unity project

---
+++ README.md
@@ -0,0 +1,2 @@
+# platformer-game
+It's a platformer game that's currently under development, it's more about giving me a chance to practice making a game than turning it into a major project.
---
+++ TutoUnity6/Assets/Scenes/Scene principal/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController cc;
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravity;
+    private Vector3 moveDir;
+
+    void Update()
+    {   
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Debug.Log("H: " + h + " V: " + v);
+
+        moveDir.x = v * moveSpeed;
+        moveDir.z = -h * moveSpeed;
+
+        if (cc.isGrounded)
+        {
+            moveDir.y = -1f;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDir.y = jumpForce;
+            }
+        }
+        else
+        {
+            moveDir.y -= gravity * Time.deltaTime;
+        }
+
+        cc.Move(moveDir * Time.deltaTime);
+    }
+}
